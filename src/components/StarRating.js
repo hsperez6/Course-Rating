1f7498ec2f34344d8c7bd6d0fa0ef97426395961
hsperez6 <hsperez@gmail.com>
2023-08-27 13:39:43 -0700

    Course Rating completed

---
+++ src/components/StarRating.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import Star from './Star';
+
+
+const StarRating = () => {
+    // Initialize a 'courseRating' state
+    const [courseRating, setCourseRating] = useState(0);
+
+    // Write a function that returns 5 Star components
+    const renderStars = () => {
+        let stars = [];
+        const maxRating = 5;
+        for (let i=0; i<maxRating; i++) {
+            stars.push(<Star 
+                key={i}
+                setRating={() => handleSetRating(i+1)}
+                isSelected={courseRating > i}
+            />)
+        };
+
+        return stars;
+    };
+    
+
+
+    // Write an event handler that updates the courseRating state.
+    // Pass the function to a Star component via props
+
+    const handleSetRating = (rating) => {
+
+        if (courseRating === rating) {
+            setCourseRating(0)
+        } else {
+            setCourseRating(rating)
+        }
+
+    }
+    
+    
+
+    return (
+        <ul className='course--stars'>
+            {/* Render the Star components */}
+            {renderStars()}
+        </ul>
+    );
+
+};
+
+export default StarRating;
